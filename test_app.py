57cdaf7fae6fd11b50ceb1fc12d1d9c6bb72915a
VAB-8350 <v.barilin@itecriocuarto.org.ar>
2021-09-13 22:08:03 -0300

    3 test

--- test_app.py
+++ test_app.py
@@ -1,7 +1,11 @@
 from function import formulate, grouper
 from read_number import read_number
 
-def test_answer():
+def test_formulate():
     assert formulate('123 ') == '321'
+
+def test_grouper():
     assert grouper(3, '123234') == ['123', '234']
+
+def test_read_mumber():
     assert read_number('123234') == 'ciento veintitres mil doscientos treinta y cuatro'
